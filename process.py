ae695461e31b76658319dc8647605ca334f4bf55
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-07-14 15:44:02 +0200

    draw graph, add color, add transition/activity count

--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+import pygraphviz as pgv
+
 file = open("data/event log.csv", "r")
 #
 # log = []
@@ -19,14 +21,80 @@ file = open("data/event log.csv", "r")
 #    print(caseId, task, user, timestamp)
 
 log = dict()
-controlFlow = dict()
-
+transitionMatrix = dict()
+activityCount = dict()
 
 def main():
     readFile()
     sortLog()
     getControlFlow()
     printControlFlow()
+
+    getActivityCount()
+
+    print('Activity count:')
+    for activity in sorted(activityCount.keys()):
+        print(activity, ' count:', activityCount[activity])
+
+    generateGraph(activityCount)
+
+
+def getActivityCount():
+    for caseId in log:
+        for activity in log[caseId]:
+            if activity[0] not in activityCount:
+                activityCount[activity[0]] = 0
+            activityCount[activity[0]] += 1
+
+
+def generateGraph(activityCount):
+    G = pgv.AGraph(strict=False, directed=True)
+    G.graph_attr['rankdir'] = 'LR'
+    G.node_attr['shape'] = 'box'
+
+    addNodes(G, activityCount)
+    addEdges(G)
+
+    G.draw('graph.png', prog='dot')
+    #print(G.string())
+
+
+def addEdges(G):
+    values = [transitionMatrix[ai][aj] for ai in transitionMatrix for aj in transitionMatrix[ai]]  # list comprehension
+    x_min = min(values)
+    x_max = max(values)
+    y_min = 1.0
+    y_max = 5.0
+    for ai in transitionMatrix:
+        for aj in transitionMatrix[ai]:
+            x = transitionMatrix[ai][aj]  # transition count
+            y = y_min + (y_max - y_min) * (x - x_min) / (x_max - x_min)  # edge thickness
+            G.add_edge(ai, aj, label=transitionMatrix[ai][aj], penwidth=y)
+
+
+def addNodes(G, activityCount):
+    x_min = min(activityCount.values())
+    x_max = max(activityCount.values())
+    print(x_min, x_max)
+
+    for activity in activityCount:
+        text = activity + '\n count: ' + str(activityCount[activity])
+        grayLevel = int(float(x_max - activityCount[activity]) / float(x_max - x_min) * 100.)
+        fillColor = getFillColor(grayLevel)
+        fontColor = getFontColor(grayLevel)
+        G.add_node(activity, label=text, style='filled', fillcolor=fillColor, fontcolor=fontColor)
+
+
+def getFontColor(grayLevel):
+    fontColor = 'black'
+    if grayLevel < 50:
+        fontColor = 'white'
+    return fontColor
+
+
+def getFillColor(grayLevel):
+    fillColor = 'gray' + str(grayLevel)
+    return fillColor
 
 
 def readFile():
@@ -59,7 +127,7 @@ def getActivity(caseId, taskNumber):
 
 
 def incrementTransitionCount(fromNode, toNode):
-    controlFlow[fromNode][toNode] += 1
+    transitionMatrix[fromNode][toNode] += 1
 
 
 def getControlFlow():
@@ -68,17 +136,17 @@ def getControlFlow():
         for i in range(len(log[caseId]) - 1):
             ai = getActivity(caseId, i)
             aj = getActivity(caseId, i + 1)
-            if ai not in controlFlow:
-                controlFlow[ai] = dict()
-            if aj not in controlFlow[ai]:
-                controlFlow[ai][aj] = 0
+            if ai not in transitionMatrix:
+                transitionMatrix[ai] = dict()
+            if aj not in transitionMatrix[ai]:
+                transitionMatrix[ai][aj] = 0
             incrementTransitionCount(ai, aj)
 
 
 def printControlFlow():
-    for ai in sorted(controlFlow.keys()):
-        for aj in sorted(controlFlow[ai].keys()):
-            print(ai, '-->', aj, ':', controlFlow[ai][aj])
+    for ai in sorted(transitionMatrix.keys()):
+        for aj in sorted(transitionMatrix[ai].keys()):
+            print(ai, '-->', aj, ':', transitionMatrix[ai][aj])
 
 
 if __name__ == '__main__':
