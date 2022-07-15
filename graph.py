2aa6847461cd272a49828077a401cc9471f9e5ac
Yiyao Chen <yiyaoc@ifi.uio.no>
2022-07-15 09:41:41 +0200

    divide script into main, reader, graph

--- graph.py
+++ graph.py
@@ -1,47 +1,16 @@
 import pygraphviz as pgv
+import reader
 
-file = open("data/event log.csv", "r")
-#
-# log = []
-#
-# for line in file:
-#     line = line.strip()
-#     if len(line) == 0:
-#         continue
-#     parts = line.split(";")
-#     caseId = parts[0]
-#     task = parts[1]
-#     user = parts[2]
-#     timestamp = parts[3]
-#     event = (caseId, task, user, timestamp)
-#     log.append(event)
-#
-# log.sort(key=lambda event: (event[0], event[3]))
-# for (caseId, task, user, timestamp) in log:
-#    print(caseId, task, user, timestamp)
 
-log = dict()
 transitionMatrix = dict()
 activityCount = dict()
-
-def main():
-    readFile()
-    sortLog()
-    getControlFlow()
-    printControlFlow()
-
-    getActivityCount()
-
-    print('Activity count:')
-    for activity in sorted(activityCount.keys()):
-        print(activity, ' count:', activityCount[activity])
-
-    generateGraph(activityCount)
+G = pgv.AGraph(strict=False, directed=True)
 
 
 def getActivityCount():
-    for caseId in log:
-        for activity in log[caseId]:
+    print('get act count:')
+    for caseId in reader.log:
+        for activity in reader.log[caseId]:
             if activity[0] not in activityCount:
                 activityCount[activity[0]] = 0
             activityCount[activity[0]] += 1
@@ -56,7 +25,7 @@ def generateGraph(activityCount):
     addEdges(G)
 
     G.draw('graph.png', prog='dot')
-    #print(G.string())
+    # print(G.string())
 
 
 def addEdges(G):
@@ -97,43 +66,27 @@ def getFillColor(grayLevel):
     return fillColor
 
 
-def readFile():
-    # global log, caseId, task, user, timestamp
-    for line in file:
-        line = line.strip()
-        if len(line) == 0:
-            continue
-        parts = line.split(";")
-        caseId = parts[0]
-        task = parts[1]
-        user = parts[2]
-        timestamp = parts[3]
-        if caseId not in log:
-            log[caseId] = []
-        event = (task, user, timestamp)
-        log[caseId].append(event)
-    file.close()
-
-
-def sortLog():
-    for caseId in sorted(log.keys()):
-        log[caseId].sort(key=lambda event: event[-1])  # sort by timestamp
-        for (task, user, timestamp) in log[caseId]:
-            print(caseId, task, user, timestamp)
-
-
 def getActivity(caseId, taskNumber):
-    return log[caseId][taskNumber][0]
+    return reader.log[caseId][taskNumber][0]
 
 
 def incrementTransitionCount(fromNode, toNode):
     transitionMatrix[fromNode][toNode] += 1
 
 
-def getControlFlow():
+def getTransitionMatrix():
+    print('get transition matrix:')
+
+    print("m.log")
+    for caseId in reader.log:
+        print("id ", caseId)
+        for (task, user, timestamp) in reader.log[caseId]:
+            print(task, user, timestamp)
+
     # global caseId, ai, aj
-    for caseId in log:
-        for i in range(len(log[caseId]) - 1):
+    for caseId in reader.log:
+        print('caseId:', caseId)
+        for i in range(len(reader.log[caseId]) - 1):
             ai = getActivity(caseId, i)
             aj = getActivity(caseId, i + 1)
             if ai not in transitionMatrix:
@@ -143,11 +96,13 @@ def getControlFlow():
             incrementTransitionCount(ai, aj)
 
 
-def printControlFlow():
+def printTransitionMatrix():
     for ai in sorted(transitionMatrix.keys()):
         for aj in sorted(transitionMatrix[ai].keys()):
             print(ai, '-->', aj, ':', transitionMatrix[ai][aj])
 
 
-if __name__ == '__main__':
-    main()
+def printActivityCount():
+    print('Activity count:')
+    for activity in sorted(activityCount.keys()):
+        print(activity, ' count:', activityCount[activity])
---
+++ reader.py
@@ -0,0 +1,26 @@
+
+file = open("data/event log.csv", "r")
+log = dict()
+
+def readFile():
+    for line in file:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        parts = line.split(";")
+        caseId = parts[0]
+        task = parts[1]
+        user = parts[2]
+        timestamp = parts[3]
+        if caseId not in log:
+            log[caseId] = []
+        event = (task, user, timestamp)
+        log[caseId].append(event)
+    file.close()
+
+
+def sortLog():
+    for caseId in sorted(log.keys()):
+        log[caseId].sort(key=lambda event: event[-1])  # sort by timestamp
+        for (task, user, timestamp) in log[caseId]:
+            print(caseId, task, user, timestamp)
